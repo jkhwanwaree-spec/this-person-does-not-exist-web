13a7c2b36d63e9005d559c6d820ee0f9fff44183
jkhwanwaree-spec <jkhwanwaree@gmail.com>
2026-01-14 19:06:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    image_url = "https://thispersondoesnotexist.com/image"
+    return render_template("index.html", image_url=image_url)
